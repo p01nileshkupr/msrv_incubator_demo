e6ccbce98fcd5638013393ab52a1e54db2611525
Nilesh Prajapati <140526443+p01nileshkupr@users.noreply.github.com>
2024-08-15 10:30:01 +0200

    - Code formatting & Optimisation
    - Removed extra service and controller
    - Combined 2 internal apis under one service and controller.

--- src/main/java/com/nileshprajapati/incubator_demo/controller/NewsController.java
+++ src/main/java/com/nileshprajapati/incubator_demo/controller/NewsController.java
@@ -1,58 +1,53 @@
 package com.nileshprajapati.incubator_demo.controller;
 
 import com.nileshprajapati.incubator_demo.config.ApplicationConfiguration;
+import com.nileshprajapati.incubator_demo.external.apis.NewsTopHeadlinesApi;
 import com.nileshprajapati.incubator_demo.external.models.TopHeadlineResponseModel;
+import com.nileshprajapati.incubator_demo.internal.apis.NewsApi;
+import com.nileshprajapati.incubator_demo.internal.models.NewsSourcesResponse;
 import com.nileshprajapati.incubator_demo.internal.models.TopNewsHeadlineResponse;
 import com.nileshprajapati.incubator_demo.service.NewsService;
 
 import org.apache.coyote.BadRequestException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import org.springframework.web.server.ResponseStatusException;
-
 import retrofit2.Call;
 import retrofit2.Response;
 
-import javax.validation.constraints.NotNull;
 import java.io.IOException;
 
 @RestController
-@RequestMapping(path = "/api/v1/news")
-public class NewsController {
+public class NewsController implements NewsApi {
 
-    private NewsService newsService;
+    private final NewsService newsService;
 
     @Autowired
     NewsController(NewsService newsService) {
         this.newsService = newsService;
     }
 
-    @GetMapping(path = "/top-headlines", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<TopNewsHeadlineResponse> getTopHeadlines(@NotNull @RequestParam String countryCode) throws IOException {
-        if (countryCode == null || countryCode.isEmpty()) {
-            throw new BadRequestException("Country code is mandatory");
-        } else {
-            try {
-                Call<TopHeadlineResponseModel> apiCall = this.newsService.topHeadlines(countryCode, ApplicationConfiguration.APICategory.news.apiKey());
-                Response<TopHeadlineResponseModel> response = apiCall.execute();
-                if (response.isSuccessful() && response.body() != null) {
-                    TopHeadlineResponseModel object =  response.body();
-                    return ResponseEntity.ok(this.newsService.mapToMicroserviceModel(object));
-                } else {
-                    throw new ResponseStatusException(HttpStatusCode.valueOf(response.code()), response.message());
-                }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+    @Override
+    public ResponseEntity<TopNewsHeadlineResponse> topHeadlinesUsingCountryCode(String country) {
+      try {
+          if (country == null || country.isEmpty()) {
+              throw new BadRequestException("Country code is mandatory");
+          } else {
+             return this.newsService.getTopNewsHeadlines(country);
+          }
+      } catch (IOException e) {
+          throw new RuntimeException(e);
+      }
+    }
+
+    @Override
+    public ResponseEntity<NewsSourcesResponse> topNewsSources() {
+        try {
+            return this.newsService.getNewsSources();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- src/main/java/com/nileshprajapati/incubator_demo/service/NewsService.java
+++ src/main/java/com/nileshprajapati/incubator_demo/service/NewsService.java
@@ -1,61 +1,128 @@
 package com.nileshprajapati.incubator_demo.service;
 
-import com.nileshprajapati.incubator_demo.external.models.NewsArticleModel;
-import com.nileshprajapati.incubator_demo.external.models.TopHeadlineResponseModel;
+import com.nileshprajapati.incubator_demo.config.ApplicationConfiguration;
+import com.nileshprajapati.incubator_demo.external.apis.NewsSourcesApi;
 import com.nileshprajapati.incubator_demo.external.apis.NewsTopHeadlinesApi;
-import com.nileshprajapati.incubator_demo.internal.models.NewsArticle;
-import com.nileshprajapati.incubator_demo.internal.models.NewsArticleSource;
-import com.nileshprajapati.incubator_demo.internal.models.TopNewsHeadlineResponse;
+import com.nileshprajapati.incubator_demo.external.models.NewsArticleModel;
+import com.nileshprajapati.incubator_demo.external.models.NewsSourcesModel;
+import com.nileshprajapati.incubator_demo.external.models.NewsSourcesResponseModel;
+import com.nileshprajapati.incubator_demo.external.models.TopHeadlineResponseModel;
+import com.nileshprajapati.incubator_demo.internal.models.*;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import org.springframework.web.server.ResponseStatusException;
 import retrofit2.Call;
+import retrofit2.Response;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class NewsService implements NewsTopHeadlinesApi {
+public class NewsService {
 
-    private final NewsTopHeadlinesApi topHeadlinesApi;
+    private final NewsSourcesApi newsSourcesApi;
+    private final NewsTopHeadlinesApi newsTopHeadlinesApi;
 
     @Autowired
-    public NewsService(NewsTopHeadlinesApi topHeadlinesApi) {
-        this.topHeadlinesApi = topHeadlinesApi;
+    NewsService(NewsSourcesApi newsSourcesApi, NewsTopHeadlinesApi newsTopHeadlinesApi) {
+        this.newsSourcesApi = newsSourcesApi;
+        this.newsTopHeadlinesApi = newsTopHeadlinesApi;
     }
 
-    public TopNewsHeadlineResponse mapToMicroserviceModel(TopHeadlineResponseModel object) {
-        TopNewsHeadlineResponse response = new TopNewsHeadlineResponse();
+    public ResponseEntity<TopNewsHeadlineResponse> getTopNewsHeadlines(String country) throws IOException {
+        try {
+            Call<TopHeadlineResponseModel> apiCall = this.newsTopHeadlinesApi.topHeadlines(country, ApplicationConfiguration.APICategory.news.apiKey());
+            Response<TopHeadlineResponseModel> response = apiCall.execute();
+            if (response.isSuccessful() && response.body() != null) {
+                TopHeadlineResponseModel object =  response.body();
+                return ResponseEntity.ok(mapToMicroserviceNewsTopHeadlinesModel(object));
+            } else {
+                throw new ResponseStatusException(HttpStatusCode.valueOf(response.code()), response.message());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ResponseEntity<NewsSourcesResponse> getNewsSources() throws IOException {
+       try {
+           Call<NewsSourcesResponseModel> callApi = this.newsSourcesApi.newsSources(ApplicationConfiguration.APICategory.news.apiKey());
+           Response<NewsSourcesResponseModel> responseModel = callApi.execute();
+           if (responseModel.isSuccessful() && responseModel.body() != null) {
+               NewsSourcesResponseModel newsSourcesResponseModel = responseModel.body();
+               return ResponseEntity.ok(mapToMicroserviceNewsSourceModel(newsSourcesResponseModel));
+           } else {
+               throw new ResponseStatusException(HttpStatusCode.valueOf(responseModel.code()), responseModel.message());
+           }
+       } catch (Exception e) {
+           throw new RuntimeException(e);
+       }
+    }
+
+    private TopNewsHeadlineResponse mapToMicroserviceNewsTopHeadlinesModel(TopHeadlineResponseModel object) {
+        TopNewsHeadlineResponse newsHeadlineResponse = new TopNewsHeadlineResponse();
         List<NewsArticle> articles = null;
-        response.setStatus(object.getStatus());
-        response.setTotalResults(object.getTotalResults());
+        newsHeadlineResponse.setStatus(object.getStatus());
+        newsHeadlineResponse.setTotalResults(object.getTotalResults());
         if (object.getArticles() != null) {
             articles = new ArrayList<>();
-            for(NewsArticleModel articleModel: object.getArticles()) {
-                NewsArticle newsArticle = new NewsArticle();
-                newsArticle.setTitle(articleModel.getTitle());
-                newsArticle.setUrl(articleModel.getUrl());
-                newsArticle.setDescription(articleModel.getDescription());
-                newsArticle.setAuthor(articleModel.getAuthor());
-                newsArticle.setContent(articleModel.getContent());
-
-                NewsArticleSource newsArticleSource = new NewsArticleSource();
-                newsArticleSource.setId(articleModel.getSource().getId());
-                newsArticleSource.setName(articleModel.getSource().getName());
-                newsArticle.setSource(newsArticleSource);
-
-                newsArticle.setPublishedAt(articleModel.getPublishedAt());
-                newsArticle.setUrlToImage(articleModel.getUrlToImage());
+            for (NewsArticleModel articleModel : object.getArticles()) {
+                NewsArticle newsArticle = getNewsArticle(articleModel);
                 articles.add(newsArticle);
             }
+            newsHeadlineResponse.setArticles(articles);
         }
-        response.setArticles(articles);
-        return response;
+        return newsHeadlineResponse;
     }
 
-    @Override
-    public Call<TopHeadlineResponseModel> topHeadlines(String country, String apiKey) {
-        return this.topHeadlinesApi.topHeadlines(country, apiKey);
+    private static @NotNull NewsArticle getNewsArticle(NewsArticleModel articleModel) {
+        NewsArticle newsArticle = new NewsArticle();
+        newsArticle.setTitle(articleModel.getTitle());
+        newsArticle.setUrl(articleModel.getUrl());
+        newsArticle.setDescription(articleModel.getDescription());
+        newsArticle.setAuthor(articleModel.getAuthor());
+        newsArticle.setContent(articleModel.getContent());
+
+        NewsArticleSource newsArticleSource = new NewsArticleSource();
+        newsArticleSource.setId(articleModel.getSource().getId());
+        newsArticleSource.setName(articleModel.getSource().getName());
+        newsArticle.setSource(newsArticleSource);
+
+        newsArticle.setPublishedAt(articleModel.getPublishedAt());
+        newsArticle.setUrlToImage(articleModel.getUrlToImage());
+        return newsArticle;
+    }
+
+    private NewsSourcesResponse mapToMicroserviceNewsSourceModel(NewsSourcesResponseModel object) {
+        NewsSourcesResponse newsSourcesResponse = new NewsSourcesResponse();
+        newsSourcesResponse.setStatus(object.getStatus());
+        List<NewsSource> newsSources = null;
+        if (object.getSources() != null) {
+            newsSources = new ArrayList<>();
+            for (NewsSourcesModel newsSourcesModel : object.getSources()) {
+                NewsSource newsSource = getNewsSource(newsSourcesModel);
+                newsSources.add(newsSource);
+            }
+        }
+        newsSourcesResponse.setSources(newsSources);
+        return newsSourcesResponse;
+    }
+
+    private static @NotNull NewsSource getNewsSource(NewsSourcesModel newsSourcesModel) {
+        NewsSource newsSource = new NewsSource();
+        newsSource.setId(newsSourcesModel.getId());
+        newsSource.setName(newsSourcesModel.getName());
+        newsSource.setUrl(newsSourcesModel.getUrl());
+        newsSource.setDescription(newsSourcesModel.getDescription());
+        newsSource.setCategory(newsSourcesModel.getCategory());
+        newsSource.setCountry(newsSourcesModel.getCountry());
+        newsSource.setLanguage(newsSourcesModel.getLanguage());
+        return newsSource;
     }
 }
